97431e9abe3620ba5b7f2b349bcbe00c117df482
Lais_Nuto_Rossman <laisnuto@gmail.com>
2024-06-28 20:23:24 -0300

    try config test

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,22 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
+  preset: "ts-jest",
+  moduleNameMapper: {
+    // if your using tsconfig.paths thers is no harm in telling jest
+    "@components/(.*)$": "<rootDir>/src/components/$1",
+    "@/(.*)$": "<rootDir>/src/$1",
+  },
+  transform: {
+    '^.+\\.(ts|tsx)$': 'ts-jest', // Transform TypeScript files
+    '^.+\\.(js|jsx)$': 'babel-jest' // Transform JavaScript files
+  },
+  transformIgnorePatterns: [
+    'node_modules/*' // Replace "module-name" with actual module names
+  ],
+  // to obtain access to the matchers.
+  setupFilesAfterEnv: ["./setupTests.ts"],
+
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  modulePaths: ["<rootDir>"],
+  testEnvironment: "jsdom",
 };
--- tests/ArtistPath.test.tsx
+++ tests/ArtistPath.test.tsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import ArtistPath from '../src/components/artists/ArtistsPath';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ArtistPath from "../src/components/artists/ArtistsPath";
 
-
-jest.mock('react-i18next', () => ({
-  useTranslation: () => ({
-    t: (key: string) => key,
-  }),
+jest.mock("react-i18next", () => ({
+  useTranslation: () => {
+    return {
+      t: (str: string): string => str,
+    };
+  },
 }));
 
-describe('ArtistPath', () => {
-  it('renders without crashing', () => {
-    const path = [{ id: '1', name: 'Artist 1', images: [], popularity: 10 }];
+describe("ArtistPath", () => {
+  it("renders without crashing", () => {
+    const path = [{ id: "1", name: "Artist 1", images: [], popularity: 10 }];
     render(<ArtistPath path={path} setArtists={jest.fn()} />);
-    expect(screen.getByText('Artist 1')).toBeInTheDocument();
+    expect(screen.getByText("Artist 1")).toBeInTheDocument();
   });
 });
